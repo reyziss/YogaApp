052f0cc3e198d2bbb315c0f38ee47f2ca27cf00d
reyziss <46215218+reyziss@users.noreply.github.com>
2022-01-31 15:11:10 +0300

    Add files via upload

---
+++ YogaApp_Krasyuk_Komarov/YogaApp_Krasyuk_Komarov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YogaApp_Krasyuk_Komarov
+//
+//  Created by Student on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
